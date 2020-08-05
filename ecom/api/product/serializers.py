b71a87db7386e36b48e15167aab725f14b98b2f5
Amit kumar <amitkumar1000091@gmail.com>
2020-08-05 09:39:12 +0530

    produts add and image root fixed

---
+++ ecom/api/product/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import Products
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image = serializers.ImageField(
+        max_length=None, allow_empty_file=False, allow_null=True, required=False)
+
+    class Meta:
+        model = Products
+        fields = ('id', 'name', 'description', 'price', 'stock',
+                  'isActive', 'image', 'category', 'created_at', 'updated_at')
--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Products
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Products.objects.all().order_by('id')
+    serializer_class = ProductSerializer
